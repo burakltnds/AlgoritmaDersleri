91c7ccee9abfa3f9e1c6964c5269b22d81355c1b
Burak Altundaş <156445457+burakltnds@users.noreply.github.com>
2025-01-12 21:39:32 +0300

    Update QuickSort.c

--- QuickSort.c
+++ QuickSort.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
 
-int quickSort(int arr[], int bas, int son) {
-    int pivot = arr[son]; // Pivot olarak son eleman seçiliyor
-    int i = bas - 1;      // Küçük elemanların konumunu takip eden değişken
-
-    for (int j = bas; j < son; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            // Elemanları yer değiştir
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        // Pivot işlemi
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // Elemanları yer değiştir
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
+        // Pivot elemanını doğru pozisyona yerleştir
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        int partitionIndex = i + 1;
+
+        quickSort(arr, low, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, high);
     }
-
-    // Pivot elemanını doğru yerine yerleştir
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[son];
-    arr[son] = temp;
-
-    return i + 1;
 }
 
-// Quick Sort algoritması
-void quickSort(int arr[], int bas, int son) {
-    if (bas < son) {
-        // Pivot elemanını bul
-        int pi = partition(arr, bas, son);
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-        // Pivotun solunda ve sağında sıralama işlemini yap
-        quickSort(arr, bas, pi - 1);
-        quickSort(arr, pi + 1, son);
+    quickSort(arr, 0, n - 1);
+
+    printf("Sıralı dizi: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
+
+    return 0;
 }
